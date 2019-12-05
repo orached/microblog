d43189a39cc7d84e36c978971dbdd176ff265402
orached <orached@gmail.com>
2019-12-05 16:43:48 +0100

    Added a new page for posts edition

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -31,7 +31,7 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('posts.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title=_('Home'), form=form,
+    return render_template('edit_post.html', title=_('Posts'), form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
