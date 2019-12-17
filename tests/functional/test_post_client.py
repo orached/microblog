6fc4bc3bf08cf6aeed574efad7d3a8cda5d867fe
orached <orached@gmail.com>
2019-12-17 14:21:56 +0100

    test post

--- tests/functional/test_post_client.py
+++ tests/functional/test_post_client.py
@@ -16,5 +16,23 @@ def test_managepost_page(setUp, populate_db):
     response = setUp.post('/managepost', data={
         'title': 'My second post',
         'post': 'Amazing content here'
-    })
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Your post is now live!" in response.data
+
+def test_editpost_page(setUp, populate_db):
+    response = setUp.get('/editpost/1')
     assert response.status_code == 302
+    # login with john's account
+    response = setUp.post('/auth/login', data={
+        'username': 'john',
+        'password': 'cat'
+    }, follow_redirects=True)
+    response = setUp.get('/editpost/1', follow_redirects=True)
+    assert b"Edit post" in response.data
+    response = setUp.post('/editpost', data={
+        'title': 'My second post',
+        'post': 'Amazing content here'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Your post is now live!" in response.data
