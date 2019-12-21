447c725005ec6442805bd82ad051c74477280897
orached <orached@gmail.com>
2019-12-21 19:18:57 +0100

    Config for Prod

--- config.py
+++ config.py
@@ -11,17 +11,13 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    #MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    #MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    #Config from Flasky
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['admin@orached.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
